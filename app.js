8332b99974616997fbeb5be0c4156d1f2a8918f1
Yavuzhan İS <119755945+yavuzhanis@users.noreply.github.com>
2023-05-04 15:27:51 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+//Kelime Sayısı Bulma Uygulaması:
+
+//Kullanıcıdan Harfleri girmesini istedik.
+let metin = "Şu anda Yalova'da javascript eğitimi çekmekteyim";
+let harf = prompt("Harfi Giriniz:");
+
+// let sonuc = bul(harf);
+// alert("Harf Sayısı: " + sonuc);
+bul("a");
+bul("e");//farklı sonuc alma şekilleri.
+bul("s");
+function bul(harf) { //fonksiyonu tanımladık
+    let toplam = 0;
+    for (let i = 0; i < metin.length; i++) { //for döngüsü tanımladık
+        if (metin.charAt(i) == harf) { //charAt() kullanarak harf değerlerini döndürdük
+            toplam += 1;
+        }
+    }
+    console.log("Harf Sayisi:" + toplam);
+    // return toplam
+}
+
